843edbf2373440d5b39236d494cf8f37f7904db5
Md Monerujamman Munna <mdmonerujammanmunna@gmail.com>
2026-04-24 18:04:19 +0600

    add card design with common image

--- src/Components/CardData.jsx
+++ src/Components/CardData.jsx
@@ -1,8 +1,30 @@
+import { FaDownload } from "react-icons/fa6";
+import { FaStar } from "react-icons/fa";
+import Image from "next/image";
 
 const CardData = ({ singleData }) => {
     return (
         <>
-
+            <div className="card bg-base-100 shadow-sm">
+                <figure>
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                        alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="text-xl">{`${singleData.title} : ${singleData.companyName}`}</h2>
+                    <div className="flex items-center justify-between">
+                        <div className="text-[#00D390] py-1.5 px-2.5 bg-[#F1F5E8] rounded-sm font-semibold flex items-center gap-2">
+                            <FaDownload />
+                            <span>{singleData.downloads}</span>
+                        </div>
+                        <div className="text-[#FF8811] py-1.5 px-2.5 bg-[#FFF0E1] rounded-sm font-semibold flex items-center gap-2">
+                            <FaStar />
+                            <spanp>5</spanp>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
--- src/app/Trending Apps/page.jsx
+++ src/app/Trending Apps/page.jsx
@@ -10,7 +10,7 @@ const TrendingAppsPage = async () => {
                 <h1 className='text-4xl font-bold text-[#001931]'>Trending Apps</h1>
                 <p className='text-xl mt-4 text-[#627382] pb-10'>Explore All Trending Apps on the Market developed by us</p>
             </div>
-            <div className="">
+            <div className="grid md:grid-cols-2 lg:grid-cols-4 px-5 lg:px-10 gap-4 bg-base-300">
                 {
                     res.map(singleData => <CardData key={singleData.id} singleData={singleData}></CardData>)
                 }
