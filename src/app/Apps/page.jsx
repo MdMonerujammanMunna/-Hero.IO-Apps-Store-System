172c931f5e6409f4701748f325db00509be9383d
Md Monerujamman Munna <mdmonerujammanmunna@gmail.com>
2026-05-08 22:23:50 +0600

    add Search Bar function

--- src/app/Apps/page.jsx
+++ src/app/Apps/page.jsx
@@ -1,9 +1,14 @@
 import CardData from "@/Components/AppsCardUi/CardData";
 
 
-const AppsPage = async () => {
+const AppsPage = async ({ searchParams }) => {
+    const { SearchBar } = await searchParams;
+    console.log(SearchBar)
     const Data = await fetch("https://hero-io-apps-store-system-server.onrender.com/MainData")
     const res = await Data.json()
+
+    const SearchBarFilter = SearchBar ? res.filter(singleObj => singleObj.title.toLowerCase() == SearchBar.toLowerCase()) : res;
+
     return (
         <>
             <div className="py-20 bg-base-300 px-5 lg:px-10 ">
@@ -16,25 +21,27 @@ const AppsPage = async () => {
                         <span>({res.length})</span>
                         <p>Apps Found</p>
                     </div>
-                    <label className="input">
-                        <svg className="h-[1.5em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                            <g
-                                strokeLinejoin="round"
-                                strokeLinecap="round"
-                                strokeWidth="2.5"
-                                fill="none"
-                                stroke="currentColor"
-                            >
-                                <circle cx="11" cy="11" r="8"></circle>
-                                <path d="m21 21-4.3-4.3"></path>
-                            </g>
-                        </svg>
-                        <input type="search" required placeholder="search Apps" />
-                    </label>
+                    <form>
+                        <label className="input">
+                            <svg className="h-[1.5em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <g
+                                    strokeLinejoin="round"
+                                    strokeLinecap="round"
+                                    strokeWidth="2.5"
+                                    fill="none"
+                                    stroke="currentColor"
+                                >
+                                    <circle cx="11" cy="11" r="8"></circle>
+                                    <path d="m21 21-4.3-4.3"></path>
+                                </g>
+                            </svg>
+                            <input type="search" name="SearchBar" placeholder="search Apps" />
+                        </label>
+                    </form>
                 </div>
                 <div className="grid md:grid-cols-2 lg:grid-cols-4 px-5 lg:px-10 gap-4">
                     {
-                        res.map(singleData => <CardData key={singleData.id} singleData={singleData}></CardData>)
+                        SearchBarFilter.map(singleData => <CardData key={singleData.id} singleData={singleData}></CardData>)
                     }
                 </div>
             </div>
