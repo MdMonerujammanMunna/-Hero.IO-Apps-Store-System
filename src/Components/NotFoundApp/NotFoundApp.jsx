2cb17267a1ff25f0e8ccf708fc57046f15e30ac6
Md Monerujamman Munna <mdmonerujammanmunna@gmail.com>
2026-05-28 23:22:49 +0600

    add Not Found app page

--- src/Components/NotFoundApp/NotFoundApp.jsx
+++ src/Components/NotFoundApp/NotFoundApp.jsx
@@ -1,10 +1,19 @@
+import Image from 'next/image';
 import React from 'react';
-
+import NotFoundImage from '../../assets/App-Error.png';
+import Link from 'next/link';
 const NotFoundApp = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className="flex flex-col items-center justify-center ">
+                <Image src={NotFoundImage} alt="Not Found" width={400} height={400} className="mx-auto" />
+                <h1 className="text-5xl font-bold text-center mt-4 text-[#001931]">OPPS!! APP NOT FOUND</h1>
+                <p className="text-lg text-center text-[#627382] mt-2">The App you are requesting is not found on our system.  please try another apps</p>
+                <Link href="/Apps" className="btn mt-5 text-white bg-[linear-gradient(90deg,rgba(99,46,227,1)_0%,rgba(159,98,242,1)_100%)] gap-2.5">
+                    <span className="font-semibold">Go Back!</span>
+                </Link>
+            </div>
+        </>
     );
 };
 
--- src/app/install/page.jsx
+++ src/app/install/page.jsx
@@ -1,4 +1,5 @@
 "use client"
+import NotFoundApp from "@/Components/NotFoundApp/NotFoundApp";
 import { SortBar } from "@/Components/Sort/Sort";
 import { installAppContext } from "@/context/installcontext";
 import Image from "next/image";
@@ -18,47 +19,57 @@ const InstallationPage = () => {
     return (
         <>
             <div className="bg-[#F5F5F5] py-20">
-                <div className="text-center mb-10">
-                    <h1 className="text-5xl text-[#001931] font-bold ">Your Installed Apps</h1>
-                    <p className="text-[#627382] text-xl mt-4">Explore All Trending Apps on the Market developed by us</p>
-                </div>
-                <div className="px-5 lg:px-10 ">
-                    <p className="text-[#001931] text-2xl font-semibold">{InstallApp.length} Apps Found</p>
-                    <div className="">
-                        {/* <SortBar /> */}
-                    </div>
-
-                    {InstallApp.map(item =>
-                        <div className="bg-white p-4 rounded-lg mb-4 flex items-center justify-between " key={item.id}>
-                            <div className="flex items-center gap-4">
-                                <div className="w-20 h-20 rounded-2xl bg-[#D9D9D9] flex items-center justify-center">
-                                    <Image src={item.image} alt={item.title} width={50} height={50} className="rounded-lg" />
-                                </div>
+                {InstallApp.length === 0 ?
+                    <>
+                        <NotFoundApp />
+                    </>
+                    :
+                    <>
+                        <div className="">
+                            <div className="text-center mb-10">
+                                <h1 className="text-5xl text-[#001931] font-bold ">Your Installed Apps</h1>
+                                <p className="text-[#627382] text-xl mt-4">Explore All Trending Apps on the Market developed by us</p>
+                            </div>
+                            <div className="px-5 lg:px-10 ">
+                                <p className="text-[#001931] text-2xl font-semibold">{InstallApp.length} Apps Found</p>
                                 <div className="">
-                                    <h2 className="text-xl font-semibold text-[#001931]">{item.title} : <span className="font-normal">{item.companyName}</span></h2>
-                                    <div className="flex items-center gap-4 mt-2">
-                                        <div className="text-[#00D390]  font-semibold flex items-center gap-2">
-                                            <FaDownload />
-                                            <span>{item.downloads}</span>
-                                        </div>
-                                        <div className="text-[#FF8811]  font-semibold flex items-center gap-2">
-                                            <FaStar />
-                                            <span>{item.ratingAvg}</span>
+                                    {/* <SortBar /> */}
+                                </div>
+
+                                {InstallApp.map(item =>
+                                    <div className="bg-white p-4 rounded-lg mt-8 shadow-2xl mb-4 flex items-center justify-between " key={item.id}>
+                                        <div className="flex items-center gap-4">
+                                            <div className="w-20 h-20 rounded-2xl bg-[#D9D9D9] flex items-center justify-center">
+                                                <Image src={item.image} alt={item.title} width={50} height={50} className="rounded-lg" />
+                                            </div>
+                                            <div className="">
+                                                <h2 className="text-xl font-semibold text-[#001931]">{item.title} : <span className="font-normal">{item.companyName}</span></h2>
+                                                <div className="flex items-center gap-4 mt-2">
+                                                    <div className="text-[#00D390]  font-semibold flex items-center gap-2">
+                                                        <FaDownload />
+                                                        <span>{item.downloads}</span>
+                                                    </div>
+                                                    <div className="text-[#FF8811]  font-semibold flex items-center gap-2">
+                                                        <FaStar />
+                                                        <span>{item.ratingAvg}</span>
+                                                    </div>
+                                                    <div className="">
+                                                        <button className="text-[#627382]  font-medium ">
+                                                            <span>{item.size}</span> MB
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                         <div className="">
-                                            <button className="text-[#627382]  font-medium ">
-                                                <span>{item.size}</span> MB
-                                            </button>
+                                            <button onClick={() => RemoveHandaler(item.id)} className="btn bg-[#00D390] text-white">Uninstall</button>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
-                            <div className="">
-                                <button onClick={() => RemoveHandaler(item.id)} className="btn bg-[#00D390] text-white">Uninstall</button>
+                                )}
                             </div>
                         </div>
-                    )}
-                </div>
+                    </>}
+
             </div>
         </ >
     );
