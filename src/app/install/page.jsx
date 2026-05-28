7a4243ad8b3b7623c32e4e7cda7d5a11c8c3a136
Md Monerujamman Munna <mdmonerujammanmunna@gmail.com>
2026-05-28 23:06:22 +0600

    add Uninstall function

--- src/app/install/page.jsx
+++ src/app/install/page.jsx
@@ -1,10 +1,20 @@
 "use client"
 import { SortBar } from "@/Components/Sort/Sort";
 import { installAppContext } from "@/context/installcontext";
+import Image from "next/image";
 import { useContext } from 'react';
+import { FaDownload, FaStar } from "react-icons/fa6";
 
 const InstallationPage = () => {
-    const { InstallApp } = useContext(installAppContext);
+    const { InstallApp, setInstallApp } = useContext(installAppContext);
+    const RemoveHandaler = (id) => {
+        const confirmDelete = window.confirm("Are you sure you want to uninstall this app?");
+        if (confirmDelete) {
+            const updatedApps = InstallApp.filter(item => item.id !== id);
+            setInstallApp(updatedApps);
+        }
+
+    }
     return (
         <>
             <div className="bg-[#F5F5F5] py-20">
@@ -18,8 +28,37 @@ const InstallationPage = () => {
                         {/* <SortBar /> */}
                     </div>
 
+                    {InstallApp.map(item =>
+                        <div className="bg-white p-4 rounded-lg mb-4 flex items-center justify-between " key={item.id}>
+                            <div className="flex items-center gap-4">
+                                <div className="w-20 h-20 rounded-2xl bg-[#D9D9D9] flex items-center justify-center">
+                                    <Image src={item.image} alt={item.title} width={50} height={50} className="rounded-lg" />
+                                </div>
+                                <div className="">
+                                    <h2 className="text-xl font-semibold text-[#001931]">{item.title} : <span className="font-normal">{item.companyName}</span></h2>
+                                    <div className="flex items-center gap-4 mt-2">
+                                        <div className="text-[#00D390]  font-semibold flex items-center gap-2">
+                                            <FaDownload />
+                                            <span>{item.downloads}</span>
+                                        </div>
+                                        <div className="text-[#FF8811]  font-semibold flex items-center gap-2">
+                                            <FaStar />
+                                            <span>{item.ratingAvg}</span>
+                                        </div>
+                                        <div className="">
+                                            <button className="text-[#627382]  font-medium ">
+                                                <span>{item.size}</span> MB
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="">
+                                <button onClick={() => RemoveHandaler(item.id)} className="btn bg-[#00D390] text-white">Uninstall</button>
+                            </div>
+                        </div>
+                    )}
                 </div>
-                {/* {InstallApp.map(item => <p key={item.id}>{item.title}</p>)} */}
             </div>
         </ >
     );
